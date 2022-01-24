8ee3738d00d5dbe77db2fddbadfcea17ab9e970e
khunnaball <kieranhunnaball@gmail.com>
2022-01-24 17:05:17 +0000

    Changes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-    purge: {
-        //enable remove unused CSS only in production
-        enabled: process.env.NODE_ENV === 'production',
-        //any file that may contain the reference of CSS styles by class name.
-        content: [
-            'components/**/*.vue',
-            'layouts/**/*.vue',
-            'pages/**/*.vue',
-            'plugins/**/*.js',
-            'nuxt.config.js'
-        ]
-    },
+    content: [
+        './components/**/*.{vue,js}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
+        './nuxt.config.{js,ts}',
+    ],
     darkMode: 'class',
     theme: {
         fontFamily: {
@@ -19,6 +14,7 @@ module.exports = {
         fontWeight: {
             light: 300,
             medium: 500,
+            hard: 700,
         },
     },
     variants: {
